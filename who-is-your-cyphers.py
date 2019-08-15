7e46a78d143b1aa4f002e0e3083c0f8288fbeed2
ubless607 <tjdwo7friend@naver.com>
2019-08-15 11:14:53 +0900

    Added player style

--- who-is-your-cyphers.py
+++ who-is-your-cyphers.py
@@ -16,8 +16,8 @@ def get_playerid():
         global user
         user = input("닉네임을 입력하세요: ")
         url = "https://api.neople.co.kr/cy/players?nickname=" + user + "&apikey=RvYjrC3nMgBprP2Z7KmPsfSGEikysfhW"
-        url_json = requests.get(url).json()
-        playerid = (url_json['rows'][0]['playerId'])
+        dict = requests.get(url).json()
+        playerid = (dict['rows'][0]['playerId'])
         return playerid
     except IndexError:
         print("닉네임이 존재하지 않습니다.")
@@ -37,14 +37,14 @@ def get_playerstatistics(username):
         url = "https://api.neople.co.kr/cy/players/" + username + "/matches?gameTypeId=normal&startDate=" + str(pastDatetime) + "&endDate=" + str(nowDatetime) + "&limit=100&apikey=RvYjrC3nMgBprP2Z7KmPsfSGEikysfhW"
     dict = requests.get(url).json()
 
-    url2 = "https://api.neople.co.kr/cy/players/" + username + "?apikey=RvYjrC3nMgBprP2Z7KmPsfSGEikysfhW"
-    dict2 = requests.get(url2).json()
+    url = "https://api.neople.co.kr/cy/players/" + username + "?apikey=RvYjrC3nMgBprP2Z7KmPsfSGEikysfhW"
+    dict2 = requests.get(url).json()
 
     print("")
     print("** %s님의 프로필 **" % user)
-    print("급수 : %s" % dict2['grade'])
+    print("급수: %s" % dict2['grade'])
     if dict2['ratingPoint'] is None:
-        print("티어: 언랭")
+        print("티어: Unranked")
     else:
         print("티어: %s (%s)" % (dict2['ratingPoint'], dict2['tierName']))
 
@@ -59,12 +59,65 @@ def get_playerstatistics(username):
         print("전적이 존재하지 않습니다.")
         os._exit(1)
 
-    ch_list = Counter(ch_list)
+    time_count = []
+    try:
+        for i in range(0, 100):
+            time = dict['matches']['rows'][i]['date'][11:13]
+            time_count.append(time)
+    except IndexError:
+        pass
+    time_count = Counter(time_count).most_common(1)
+
+    party_count = []
+    try:
+        for i in range(0, 100):
+            count = dict['matches']['rows'][i]['playInfo']['partyUserCount']
+            party_count.append(count)
+    except IndexError:
+        pass
+    party_count = Counter(party_count).most_common(1)
+
+    matchid_list = []
+    try:
+        for i in range(0, 100):
+            count = dict['matches']['rows'][i]['matchId']
+            matchid_list.append(count)
+    except IndexError:
+        pass
+
+    position_count = []
+    for matchid in matchid_list:
+        url = "https://api.neople.co.kr/cy/matches/" + matchid + "?apikey=RvYjrC3nMgBprP2Z7KmPsfSGEikysfhW"
+        temp_dict = requests.get(url).json()
+        try:
+            for i in range(0, 10):
+                if user == temp_dict['players'][i]['nickname']:
+                    count = temp_dict['players'][i]['position']['name']
+                    position_count.append(count)
+        except IndexError:
+            pass
+    position_count2 = Counter(position_count).most_common()
+
     print("")
+    print("** 플레이 스타일 **")
+    if party_count[0][0] == 0:
+        print("솔로 플레이어")
+    else:
+        print(str(party_count[0][0])+"인 파티 유저")
+    print('게임 시간대:', time_count[0][0]+'시')
+    print('선호 포지션:', Counter(position_count).most_common(1)[0][0])
+    position = []
+    for k, v in position_count2:
+        count = " ".join([k+':', str(round(v/len(position_count)*100, 1))+'%'])
+        position.append(count)
+    print ("[%s]" % (', '.join(position)))
+
+    ch_list = Counter(ch_list)
+    print()
     print("** 선호 캐릭터 TOP6 **")
     ch_list2 = ch_list.most_common(6)
     for k, v in ch_list2:
-        print(k, ':', v)
+        print(k+':', v)
     print("")
 
     global most
@@ -108,8 +161,9 @@ def get_playerstatistics(username):
         if most in v:
             group = k
 
-    print("** 만약 %s님이 사이퍼즈 캐릭터였다면, %s의 %s(이)였을 거예요 **" % (user, group, most))
-    print("평균 레벨 : %d" % int(most_level/most_Count))
+    print("** 모스트 캐릭터 **")
+    print("만약 %s님이 사이퍼즈 캐릭터였다면, %s의 %s(이)였을 거예요." % (user, group, most))
+    print("평균 레벨: %d" % int(most_level/most_Count))
     print("평균 킬/데스/어시: %d/%d/%d" % (int(most_killCount/most_Count), int(most_deathCount/most_Count), int(most_assistCount/most_Count)))
     print("평균 공격량: %d" % int(most_attackPoint/most_Count))
     print("평균 피해량: %d" % int(most_damagePoint/most_Count))
@@ -142,14 +196,12 @@ def get_playerstatistics(username):
         pass
 
     print("")
-    print("** 캐릭터 승률 : 3판 이상 플레이한 캐릭터만 표시 **")
+    print("** 캐릭터 승률 **")
+    print("3판 이상 플레이한 캐릭터만 표시됩니다.")
     filtered_dic = {k: v for k, v in ch_list3.items() if v[0]+v[1] >= 3}
-    if filtered_dic == {}:
-    	print("목록이 존재하지 않습니다.")
-    else:
-        sorted_filtered_dic = sorted(filtered_dic.items(), key=lambda x: x[1][2], reverse=True)
-        for k, v in sorted_filtered_dic:
-            print(k, ':', str(int(v[2]*100))+'%', '('+str(v[0])+'승', str(v[1])+'패'+')')
+    sorted_filtered_dic = sorted(filtered_dic.items(), key=lambda x: x[1][2], reverse=True)
+    for k, v in sorted_filtered_dic:
+        print(k+':', str(int(v[2]*100))+'%', '('+str(v[0])+'승', str(v[1])+'패'+')')
 
     date_list = {}
     try:
@@ -179,7 +231,7 @@ def get_playerstatistics(username):
     print("** 날짜별 승률 **")
     sorted_date_dic = sorted(date_list.items(), key=lambda x: x)
     for k, v in sorted_date_dic:
-        print(k, ':', str(int(v[2]*100))+'%', '('+str(v[0])+'승', str(v[1])+'패'+')')
+        print(k+':', str(int(v[2]*100))+'%', '('+str(v[0])+'승', str(v[1])+'패'+')')
     if len(sorted_date_dic) == 1:
         print("승률을 분석하기에는 전적이 부족합니다.")
     else:
@@ -204,7 +256,9 @@ def get_playerstatistics(username):
         m = num / den
 
         if m < 0:
-            if most == list(sorted_filtered_dic[0])[0]:
+            if filtered_dic == {}:
+                print("승률이 하락하고 있습니다. %s님의 모스트 '%s'(으)로 플레이해보는 것은 어떨까요?" % (user, most))
+            elif most == list(sorted_filtered_dic[0])[0]:
                 print("승률이 하락하고 있습니다. %s님의 모스트 '%s'(으)로 플레이해보는 것은 어떨까요?" % (user, most))
             else:
                 print("승률이 하락하고 있습니다. %s님의 모스트 '%s'이나 최근 승률이 좋은 '%s'(으)로 플레이해보는 것은 어떨까요?" % (user, most, list(sorted_filtered_dic[0])[0]))
